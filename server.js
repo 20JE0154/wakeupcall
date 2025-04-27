7857a5962f3e7bef51d45a16e6843cd58dc479ce
Ankush Kumar Gupta <79002759+20JE0154@users.noreply.github.com>
2025-04-27 19:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000 
 
 // Serve index.html
 app.use(express.static(path.join(__dirname)));
